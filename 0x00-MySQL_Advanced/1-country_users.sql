2f6bd6bf1ec01fdaa296d9a736714d2e07e6f989
martinopolot <maopolot@outlook.com>
2023-07-13 18:12:36 +0000

    1. In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Create users' table on any database
+
+CREATE TABLE IF NOT EXISTS users (
+       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       email varchar(255) NOT NULL UNIQUE,
+       name varchar(255),
+       country ENUM ('US', 'CO', 'TN') NOT NULL
+)
