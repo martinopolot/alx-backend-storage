2e74c302aa21f9b572a6eb48b590df5edeeab0db
martinopolot <maopolot@outlook.com>
2023-07-13 23:55:09 +0000

    10. Safe divide

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,14 +1,12 @@
 -- SQL script that creates a function SafeDiv
 -- And returns a / b or 0 if b == 0
-CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS INT
+DELIMITER $$
+CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS FLOAT
 BEGIN
-    DECLARE result INT;
-
     IF b = 0 THEN
-        SET result = 0;
+    RETURN 0;
     ELSE
-        SET result = a / b;
+    RETURN a / b;
     END IF;
-
-    RETURN result;
-END;
+END;$$
+DEMIMITER ;
