21bdcb9cd28253364ee67b8997636773205016bd
martinopolot <maopolot@outlook.com>
2023-07-13 18:09:34 +0000

    0. We are all unique!

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Create users' table on any database
+
+CREATE TABLE IF NOT EXISTS users (
+       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       email varchar(255) NOT NULL UNIQUE,
+       name varchar(255)
+)
